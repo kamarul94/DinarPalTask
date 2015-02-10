98b36c8382de1231ec5ad5d76a0945c3e65058a3
kamarul94 <kamarulrizam94@gmail.com>
2015-02-10 12:12:41 +0800

    task 1st

---
+++ PisangByte/index.php
@@ -0,0 +1,433 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<title>Pisang Byte</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" type="image/x-icon" href="images/logopisangicon.png" />
+	<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+	</script>
+   	<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+   	<link href="css/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
+	<script src="js/modernizr.custom.28468.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/simptip-mini.css" media="screen,projection" />
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	</head>
+	<body>
+		<!---start-wrap----->
+			<!---start-header----->
+			<div class="header" id="home">
+				<div class="wrap">
+				<div class="top-header">
+					<div class="logo">
+						<a href="index.php"><span> </span></a>
+					</div>
+					<div class="top-nav">
+						<ul>
+							<li class="active"><a href="#home" class="scroll">Home</a></li>
+							<li><a href="#services" class="scroll">Services</a>
+                            <li><a href="#portfolio" class="scroll">Portfolio</a></li>
+							<li><a href="#contact" class="scroll">Contact</a></li>
+							<div class="clear"> </div>
+						</ul>
+					</div>
+					<div class="clear"> </div>
+				</div>
+			<!---End-header----->
+			<!----start-content-slider---->
+			<div id="da-slider" class="da-slider">
+				<div id="intro" class="da-slide">
+					<div class="da-title">
+						<h3>PisangByte<br />
+							peneraju<br />
+							masa depan<br />
+							anda!</h3>
+					</div>
+					<div class="da-img">
+                    	<img src="images/pisangbyte.png" alt="Drupal Powered" title="PisangByte" />
+                   	</div>
+				</div>
+				<div id="user-experience" class="da-slide">
+					<div class="da-title">
+						<h3>DinarPal<br />
+							membuat masa<br />
+							depan anda<br />
+							lebih cerah!</h3>
+					</div>
+					<div class="da-img">
+                    	<img src="images/dinarpal.png" alt="Drupal Powered" title="DinarPal" />
+                    </div>
+				</div>
+				<div id="design" class="da-slide">
+					<div class="da-title">
+						<h3>KODINAR<br />
+							tempat anda<br />
+							berurusan<br />
+							dengan kami!</h3>
+					</div>
+				<div class="da-img">
+                	<img src="images/kodinar.png" alt="Drupal Powered" title="KODINAR" />
+                </div>
+			</div>
+			<div></div>
+		</div>
+		<script src ="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+		<script src="js/jquery.cslider.js"></script>
+		<script>
+		//jQuery(document).ready(function() {
+			$('#da-slider').cslider({
+				autoplay : true,
+				interval: 4250,
+				bgincrement : 10
+			});
+			//});
+        </script>
+		<!----start-content-slider---->
+	</div>
+</div>
+<!---End-header----->
+<!---start-content---->
+<div class="content">
+	<div class="wrap">
+	<!---start-top-grids---->
+		<div class="top-service-grids" id="services">
+			<div class="top-service-grid">
+				<a href="#"><img src="images/grip-pisang.png" alt="" /></a>
+				<a href="#"><span>PisangByte</span></a>
+				<p>pengenalan pisangbyte</p>
+			</div>
+			<div class="top-service-grid">
+				<a href="#"><img src="images/grip-dinarpal.png" alt="" /></a>
+				<a href="#"><span>DinarPal</span></a>
+				<p>DinarPal Sdn Bhd (1041509-H) is the subsidiary of Pisang Sdn Bhd (10425-K). This conglomerate knows as DinarPal Group. DinarPal group together with Koperasi DinarPal Melaka Berhad (M-12-04-04) enter into Mudharabah Agreement to run the Ar-Rahnu Exchange Business together.</p>
+			</div>
+			<div class="top-service-grid">
+				<a href="#"><img src="images/grip-kodinar.png" alt="" /></a>
+				<a href="#"><span>KODINAR</span></a>
+				<p>pengenalan kodinar</p>
+			</div>
+			<div class="clear"> </div>
+		</div>
+		<!---End-top-grids---->
+	</div>
+	<!---start-recent-works----->
+	<div class="recent-works" id="portfolio">
+		<div class="wrap">
+			<div class="recent-works-head">
+				<h3>Recent works</h3>
+			</div>
+            <!---start-mfp ---->	
+			<div id="small-dialog1" class="mfp-hide">
+				<div class="pop_up">
+					<h2>DinarPal</h2>
+					<p class="para">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</p>
+				</div>
+			</div>
+            <div id="small-dialog2" class="mfp-hide">
+				<div class="pop_up">
+					<h2>KODINAR</h2>
+					<p class="para">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</p>
+				</div>
+			</div>
+			<!---end-mfp ---->
+			<div class="gallery">
+				<div class="clear"> </div>
+					<div class="container">
+					<ul id="filters" class="clearfix">
+						<li><span class="filter active" data-filter="dpal kodi coins award all">All</span></li>
+						<li><span class="filter" data-filter="dpal awards">DinarPal</span></li>
+						<li><span class="filter" data-filter="kodi coins">KODINAR</span></li>
+						<li><span class="filter" data-filter="coins">Coins</span></li>
+						<li><span class="filter" data-filter="awards">Awards</span></li>
+					</ul>
+						<div id="portfoliolist" style="     ">
+							<div class="portfolio all mix_all" data-cat="all" style=" ">
+								<div class="portfolio-wrapper">				
+									<a class="popup-with-zoom-anim" href="#small-dialog1">
+										<img src="images/p1.png" alt="dinarpal" style="top: 0px;">
+									</a>
+									<div class="label" style="bottom: -40px;">
+										<div class="label-text">
+											<a class="text-title">DinarPal</a>
+											<span class="text-category">Logo</span>
+										</div>
+										<div class="label-bg"></div>
+									</div>
+								</div>
+							</div>				
+							<div class="portfolio dpal mix_all" data-cat="dpal" style="  display: inline-block; opacity: 1;">
+								<div class="portfolio-wrapper">			
+									<a class="popup-with-zoom-anim" href="#small-dialog1">
+										<img src="images/p2.png" alt="kodinar" style="top: 0px;">
+									</a>
+									<div class="label" style="bottom: -40px;">
+										<div class="label-text">
+											<a class="text-title">KODINAR</a>
+											<span class="text-category">Logo</span>
+										</div>
+										<div class="label-bg"></div>
+									</div>
+								</div>
+							</div>		
+							<div class="portfolio dpal mix_all" data-cat="dpal" style="  display: inline-block; opacity: 1;">
+								<div class="portfolio-wrapper">						
+									<a class="popup-with-zoom-anim" href="#small-dialog1">
+										<img src="images/p3.jpg" alt="Image 2" style="top: 0px;">
+									</a>
+									<div class="label" style="bottom: -40px;">
+										<div class="label-text">
+											<a class="text-title">Sonor's Design</a>
+											<span class="text-category">Web design</span>
+										</div>
+										<div class="label-bg"></div>
+									</div>
+								</div>
+							</div>				
+							<div class="portfolio kodi mix_all" data-cat="kodi" style="  display: inline-block; opacity: 1;">
+								<div class="portfolio-wrapper">			
+									<a class="popup-with-zoom-anim" href="#small-dialog1">
+										<img src="images/p4.jpg" alt="Image 2" style="top: 0px;">
+									</a>
+									<div class="label" style="bottom: -40px;">
+										<div class="label-text">
+											<a class="text-title">Typography Company</a>
+											<span class="text-category">Business card</span>
+										</div>
+										<div class="label-bg"></div>
+									</div>
+								</div>
+							</div>	
+									
+							<div class="portfolio coins mix_all" data-cat="coins" style="  display: inline-block; opacity: 1;">
+								<div class="portfolio-wrapper">
+									<a class="popup-with-zoom-anim" href="#small-dialog1">
+										<img src="images/p5.jpg" alt="Image 2" style="top: 0px;">
+									</a>
+									<div class="label" style="bottom: -40px;">
+										<div class="label-text">
+											<a class="text-title">Weatherette</a>
+											<span class="text-category">APP</span>
+										</div>
+										<div class="label-bg"></div>
+									</div>
+								</div>
+							</div>
+							<div class="portfolio awards mix_all" data-cat="awards" style="  display: inline-block; opacity: 1;">
+								<div class="portfolio-wrapper">			
+									<a class="popup-with-zoom-anim" href="#small-dialog1">
+										<img src="images/p6.jpg" alt="Image 2" style="top: 0px;">
+									</a>
+									<div class="label" style="bottom: -40px;">
+										<div class="label-text">
+											<a class="text-title">BMF</a>
+											<span class="text-category">Business card</span>
+										</div>
+										<div class="label-bg"></div>
+									</div>
+								</div>
+							</div>	
+						</div>
+					</div>
+                    <!-- container -->
+					<div class="clear"> </div>
+						<script type="text/javascript" src="js/fliplightbox.min.js"></script>
+						<script type="text/javascript">$('body').flipLightBox()</script>
+						<!---start-gallery-script----->
+						<script type="text/javascript" src="js/jquery.easing.min.js"></script>	
+						<script type="text/javascript" src="js/jquery.mixitup.min.js"></script>
+						<script type="text/javascript">
+							$(function () {
+								var filterList = {
+									init: function () {
+										// MixItUp plugin
+										// http://mixitup.io
+										$('#portfoliolist').mixitup({
+											targetSelector: '.portfolio',
+											filterSelector: '.filter',
+											effects: ['fade'],
+											easing: 'snap',
+											// call the hover effect
+											onMixEnd: filterList.hoverEffect()
+										});				
+									},
+								hoverEffect: function () {
+								// Simple parallax effect
+									$('#portfoliolist .portfolio').hover(
+										function () {
+											$(this).find('.label').stop().animate({bottom: 0}, 200, 'easeOutQuad');
+											$(this).find('img').stop().animate({top: -30}, 500, 'easeOutQuad');				
+										},
+									function () {
+										$(this).find('.label').stop().animate({bottom: -40}, 200, 'easeInQuad');
+										$(this).find('img').stop().animate({top: 0}, 300, 'easeOutQuad');								
+									}		
+								);				
+							}
+						};
+						// Run the show!
+						filterList.init();
+					});	
+					</script>
+					<!-- Add fancyBox main JS and CSS files -->
+					<script src="js/jquery.magnific-popup.js" type="text/javascript"></script>
+					<link href="css/magnific-popup.css" rel="stylesheet" type="text/css">
+							<script>
+								$(document).ready(function() {
+									$('.popup-with-zoom-anim').magnificPopup({
+										type: 'inline',
+										fixedContentPos: false,
+										fixedBgPos: true,
+										overflowY: 'auto',
+										closeBtnInside: true,
+										preloader: false,
+										midClick: true,
+										removalDelay: 300,
+										mainClass: 'my-mfp-zoom-in'
+								});
+							});
+							</script>
+				<!---//End-gallery-script----->
+				<div class="clear"> </div>
+			</div>
+		</div>
+	</div>
+	<!---End-recent-works----->
+    <!--- start-blog----->
+			<div class="blog">
+				<div class="wrap">
+					<div class="blog-grids">
+						<div class="blog-grid blog-grid1">
+							<div class="blog-grid-left" onclick="location.href='#';">
+								<span> </span>
+							</div>
+							<div class="blog-grid-right">
+								<h3><a href="#">DinarPal</h3>
+								<p>(dinarpal)</p>
+							</div>
+							<div class="clear"> </div>
+						</div>
+						<!----->
+						<div class="blog-grid2">
+							<div class="blog-grid2-left">
+								<h3><a href="#">kodinar</a></h3>
+								<p>(kodinar)</p>
+							</div>
+							<div class="blog-grid2-right" onclick="location.href='#';">
+								<span> </span>
+							</div>
+							<div class="clear"> </div>
+						</div>
+						<!----->
+						<div class="clear"> </div>
+					 </div>
+				</div>
+			</div>
+			<!--- End-blog----->
+	<!---start-meet-our-team---->
+	<div class="team">
+		<div class="wrap">
+			<div class="team-head">
+				<h3>Meet Our Leader</h3>
+				<p>Mereka semua adalah pengasas kepada Syarikat PisangByte dan kini telah membuka Dua anak syarikat DinarPal dan Koperasi Dinarpal Melaka Berhad.</p>
+			</div>
+			<div class="team-members">
+				<div class="team-member">
+					<span><a href="#"> </a></span>
+                    <label>Haji Azhar Bin Haji Yacoob</label>
+					<label>Chairman</label>
+					<p>pengenalan</p>
+					<ul>
+						<li>
+							<li><a class="facebook simptip-position-bottom simptip-movable" data-tooltip="Facebook" href="#"> </a></li>
+							<li><a class="twitter simptip-position-bottom simptip-movable" data-tooltip="Twitter" href="#"> </a></li>
+							<li><a class="pin simptip-position-bottom simptip-movable" data-tooltip="Pintrest" href="#"> </a></li>
+							<div class="clear"> </div>
+						</li>
+					</ul>
+				</div>
+				<div class="team-member">
+					<span><a href="#"> </a></span>
+                    <label>Rashidi Arash Bin Abdul Rashid</label>
+					<label>CEO(Group Of CEO)</label>
+					<p>pengenalan</p>
+					<ul>
+						<li>
+							<li><a class="facebook simptip-position-bottom simptip-movable" data-tooltip="Facebook" href="#"> </a></li>
+							<li><a class="twitter simptip-position-bottom simptip-movable" data-tooltip="Twitter" href="#"> </a></li>
+							<li><a class="pin simptip-position-bottom simptip-movable" data-tooltip="Pintrest" href="#"> </a></li>
+							<div class="clear"> </div>
+						</li>
+					</ul>
+				</div>
+				<div class="team-member">
+					<span><a href="#"> </a></span>
+                    <label>Mohd Zulkefli Bin Hamdan</label>
+					<label>Genaral Manager</label>
+					<p>pengenalan</p>
+					<ul>
+						<li>
+							<li><a class="facebook simptip-position-bottom simptip-movable" data-tooltip="Facebook" href="#"> </a></li>
+							<li><a class="twitter simptip-position-bottom simptip-movable" data-tooltip="Twitter" href="#"> </a></li>
+							<li><a class="pin simptip-position-bottom simptip-movable" data-tooltip="Pintrest" href="#"> </a></li>
+							<div class="clear"> </div>
+						</li>
+					</ul>
+				</div>
+				<div class="clear"> </div>
+					<div class="clear"> </div>
+					</div>
+				</div>
+			</div>
+			<!---//End-meet-our-team---->
+			<!---start-contact---->
+			<div class="contact" id="contact">
+				<div class="wrap">
+					<div class="contact-head">
+						<h3>Contact us</h3>	
+					</div>
+					<div class="contatct-form">
+						<form>
+							<input type="text" value="Name :" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Name :';}">
+							<input type="text" value="Email :" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Email :';}">
+							<textarea rows="2" cols="70" onfocus="if(this.value == 'Message :') this.value='';" onblur="if(this.value == '') this.value='Message :';" >Message :</textarea>
+							<input type="submit" value="Send" />
+						</form>
+					</div>
+				</div>
+			</div>
+			<!---End-contact---->
+		</div>
+		<!---End-content---->
+		<!----start-footer---->
+		<div class="footer">
+			<div class="wrap">
+				<div class="footer-grids">
+					<div class="footer-right">
+						<p>Copyright &copy; <a href="#">PisangByte</a></p>
+									<script type="text/javascript">
+						$(document).ready(function() {
+							$().UItoTop({ easingType: 'easeOutQuart' });
+						});
+					</script>
+					<!----move-top-path---->
+					<script type="text/javascript" src="js/move-top.js"></script>
+					<script type="text/javascript" src="js/easing.js"></script>
+					<script type="text/javascript">
+						jQuery(document).ready(function($) {
+							$(".scroll").click(function(event){		
+								event.preventDefault();
+								$('html,body').animate({scrollTop:$(this.hash).offset().top},1200);
+							});
+						});
+					</script>
+					<!----move-top-path---->
+			    	<a href="#" id="toTop" style="display: block;"><span id="toTopHover" style="opacity: 1;"></span></a>
+           		</div>
+				<div class="clear"> </div>
+			</div>
+		</div>
+	</div>
+	<!----//End-footer---->
+	<!---End-wrap----->
+</body>
+</html>
+
